8d6867e758b79a6e6b1b4517779e360d6183e719
lynxz <anders.stroemberg@gmail.com>
2018-12-02 00:17:04 +0100

    Added folders and console programs for day 2, 3, and 4.

---
+++ 2018/Day2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
