46b74f1d00e2434209f9c384c52a28698737c23c
Anders Strömberg <anders.stroemberg@gmail.com>
2018-12-05 10:43:16 +0100

    Added boiler plate for Day 5

---
+++ 2018/Day5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.FirstStar();
+        }
+
+        public void FirstStar()
+        {
+
+        }
+
+        public void SecondStar()
+        {
+
+        }
+
+        string[] GetData() => File.ReadAllLines("input");
+
+    }
+}
