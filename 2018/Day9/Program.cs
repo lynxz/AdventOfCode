6c7d20915d504b0069659b5f0113a28534ba2f22
Anders Strömberg <anders.stroemberg@gmail.com>
2018-12-09 21:14:54 +0100

    Day 9 complete

--- 2018/Day9/Program.cs
+++ 2018/Day9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day9
 {
@@ -6,7 +7,80 @@ namespace Day9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var program = new Program();
+            program.FirstStar();
         }
+
+        public void FirstStar()
+        {
+            System.Console.WriteLine(FindHighScore(70769));
+        }
+
+        long FindHighScore(int endMarble)
+        {
+            var players = new long[418];
+            var currentPlayer = 0;
+            var highMarble = 0;
+            var currentMarble = new Marble
+            {
+                Number = 0
+            };
+            currentMarble.Next = currentMarble;
+            currentMarble.Previous = currentMarble;
+            while (highMarble < endMarble)
+            {
+                currentPlayer++;
+                highMarble++;
+                if (currentPlayer >= players.Length)
+                {
+                    currentPlayer = 0;
+                }
+                if (highMarble % 23 == 0)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        currentMarble = currentMarble.Previous;
+                    }
+                    players[currentPlayer] += highMarble + currentMarble.Number;
+                    var prevMarble = currentMarble.Previous;
+                    currentMarble = currentMarble.Next;
+                    prevMarble.Next = currentMarble;
+                    currentMarble.Previous = prevMarble;
+                }
+                else
+                {
+                    var firstMarble = currentMarble.Next;
+                    var secondMarble = currentMarble.Next.Next;
+                    var newMarble = new Marble
+                    {
+                        Number = highMarble,
+                        Previous = firstMarble,
+                        Next = secondMarble
+                    };
+                    firstMarble.Next = newMarble;
+                    secondMarble.Previous = newMarble;
+                    currentMarble = newMarble;
+                }
+            }
+            return players.Max();
+        }
+
+        public void SecondStar()
+        {
+            System.Console.WriteLine(FindHighScore(70769 * 100));
+        }
+
     }
+
+    public class Marble
+    {
+
+        public int Number { get; set; }
+
+        public Marble Next { get; set; }
+
+        public Marble Previous { get; set; }
+
+    }
+
 }
