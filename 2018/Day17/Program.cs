6deb1802410a00298eca93ac798a049a890551e1
Anders Strömberg <anders.stroemberg@gmail.com>
2018-12-27 09:44:35 +0100

    Day 17 and Day 25 complete

--- 2018/Day17/Program.cs
+++ 2018/Day17/Program.cs
@@ -3,216 +3,112 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Day17
 {
+
+    public enum Element { Sand, Water, Clay, Flow, Void }
+
     class Program
     {
         static void Main(string[] args)
         {
             var program = new Program();
-            program.FirstStar();
+            program.SecondStar();
+        }
+
+        public bool Flow(char[,] map, int x, int y, int dir)
+        {
+            if (y < 1 || y >= map.GetLength(0) || x < 0 || x >= map.GetLength(1))
+            {
+                return true;
+            }
+
+            if (map[y, x] != '.')
+            {
+                return map[y, x] != '#' && map[y, x] != '~';
+            }
+
+            map[y, x] = '|';
+
+            var leaky = Flow(map, x, y + 1, 0);
+            if (leaky)
+            {
+                return true;
+            }
+
+            leaky = (dir <= 0) && Flow(map, x - 1, y, -1);
+            leaky |= (dir >= 0) && Flow(map, x + 1, y, 1);
+            if (leaky)
+            {
+                return true;
+            }
+
+            if (dir == 0)
+            {
+                Fill(map, x, y, -1);
+                Fill(map, x + 1, y, 1);
+            }
+
+            return false;
         }
 
 
-        public static void Print()
+        void Fill(char[,] map, int x, int y, int dir)
         {
-            var data = File.ReadAllLines("input");
-            var coord = data.Select(d => d.Split(", ").First()).ToList();
-            System.Console.WriteLine("x min: " + coord.Where(c => c.StartsWith("x")).Min(c => int.Parse(c.Substring(2))));
-            System.Console.WriteLine("x max: " + coord.Where(c => c.StartsWith("x")).Max(c => int.Parse(c.Substring(2))));
-            System.Console.WriteLine("y min: " + coord.Where(c => c.StartsWith("y")).Min(c => int.Parse(c.Substring(2))));
-            System.Console.WriteLine("y max: " + coord.Where(c => c.StartsWith("y")).Max(c => int.Parse(c.Substring(2))));
-        }
-
-        public enum Direction
-        {
-            Left,
-            Right
+            if (y < 1 || y >= map.GetLength(0) || x < 0 || x >= map.GetLength(1))
+            {
+                return;
+            }
+            if (map[y, x] == '|')
+            {
+                Fill(map, x + dir, y, dir);
+                map[y, x] = '~';
+            }
         }
 
         public void FirstStar()
         {
             var map = GetMap();
-            var flowStack = new Stack<(int X, int Y, Direction Direction)>();
-            flowStack.Push((500 - 465, 1, Direction.Left));
-
             try
             {
-                while (flowStack.Count != 0)
-                {
-                    var flow = flowStack.Pop();
-                    var x = flow.X;
-                    var y = flow.Y;
-                    var done = false;
-
-                    while (!done)
-                    {
-                        while (y < map.GetLength(0) && map[y, x] != '#' && map[y, x] != '~')
-                        {
-                            map[y, x] = '~';
-                            y++;
-                        }
-                        if (y == map.GetLength(0))
-                        {
-                            done = true;
-                        }
-                        else if (map[y, x] == '~')
-                        {
-                            var offset = 0;
-                            while (x - offset > 0 && map[y, x - offset] != '#' && map[y - 1, x - offset] == '~')
-                            {
-                                offset++;
-                            }
-                            if (x - offset > 0)
-                            {
-                                offset = 0;
-                                while (map[y, x] != '#')
-                                {
-                                    y++;
-                                }
-                                y--;
-                                var splashPoint = x;
-                                while (map[y, x] != '#')
-                                {
-                                    x--;
-                                }
-                                var left = x;
-                                x++;
-                                while (map[y, x] != '#')
-                                {
-                                    x++;
-                                }
-                                var right = x;
-                                x = splashPoint;
-                                while (map[y, left] == '#' && map[y, right] == '#')
-                                {
-                                    y--;
-                                    RowFill(map, x + 1, y, left, right);
-                                    RowFill(map, x - 1, y, left, right);
-                                }
-                            }
-                            done = true;
-                        }
-                        else
-                        {
-                            var splashPoint = x;
-                            y--;
-                            int left = 0;
-                            int right = 0;
-                            while (map[y, x] != '#' && x > 0)
-                            {
-                                x--;
-                            }
-                            if (x < 0)
-                            {
-                                x = splashPoint;
-                                while (map[y, x + 1] != '#' || map[y, x - 1] != '#')
-                                {
-                                    y--;
-                                }
-                                if (map[y, x + 1] == '#')
-                                {
-                                    left = x;
-                                    x++;
-                                }
-                                else
-                                {
-                                    right = x;
-                                    x--;
-                                }
-                                splashPoint = x;
-                            }
-                            if (left == 0)
-                            {
-                                while (map[y, x] != '#' && x > 0)
-                                {
-                                    x--;
-                                }
-                                left = x;
-                                x = splashPoint;
-                            }
-                            if (right == 0)
-                            {
-                                while (map[y, x] != '#')
-                                {
-                                    x++;
-                                }
-                                right = x;
-                                x = splashPoint;
-                            }
-                            var rightRowFill = false;
-                            var leftRowFill = false;
-                            do
-                            {
-                                rightRowFill = RowFill(map, x + 1, y, left, right);
-                                leftRowFill = RowFill(map, x - 1, y, left, right);
-                                y--;
-                            } while (!(leftRowFill || rightRowFill));
-                            y++;
-                            if (map[y, left] == '~' && map[y, right] == '~')
-                            {
-                                x = left - 1;
-                                flowStack.Push((right + 1, y, Direction.Right));
-                            }
-                            else if (map[y, left] == '~')
-                            {
-                                x = left - 1;
-                            }
-                            else
-                            {
-                                x = right + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine("Fail");
-                System.Console.WriteLine(e);
+                Flow(map, 500 - 460, 1, 0);
             }
             finally
             {
                 PrintMap(map);
             }
-            var counter = 6;
-            for (int y = 0; y < map.GetLength(0); y++) {
-                for (int x = 0; x < map.GetLength(1); x++) {
-                    if (map[y,x] == '~') {
-                        counter++;
+            var sum = 0;
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] == '~' || map[y, x] == '|')
+                    {
+                        sum++;
                     }
                 }
             }
-            System.Console.WriteLine(counter);
+            System.Console.WriteLine(sum);
         }
 
-        bool RowFill(char[,] map, int x, int y, int xleft, int xright)
+        public void SecondStar()
         {
-            if (map[y, x] == '.')
+            var map = GetMap();
+            Flow(map, 500 - 460, 1, 0);
+            var sum = 0;
+            for (int y = 0; y < map.GetLength(0); y++)
             {
-                map[y, x] = '~';
-                if (x == xleft || x == xright)
+                for (int x = 0; x < map.GetLength(1); x++)
                 {
-                    return true;
+                    if (map[y, x] == '~')
+                    {
+                        sum++;
+                    }
                 }
-                RowFill(map, x, y + 1, xleft, xright);
-                var left = RowFill(map, x + 1, y, xleft, xright);
-                var right = RowFill(map, x - 1, y, xleft, xright);
-                return left || right;
             }
-            return false;
-        }
-
-        void Fill(char[,] map, int x, int y)
-        {
-            if (map[y, x] == '.')
-            {
-                map[y, x] = '~';
-                Fill(map, x + 1, y);
-                Fill(map, x - 1, y);
-                Fill(map, x, y + 1);
-            }
+            System.Console.WriteLine(sum);
         }
 
         public static void PrintMap(char[,] map)
@@ -229,13 +125,13 @@ namespace Day17
                     fw.WriteLine(sb.ToString());
                 }
             }
-
         }
 
         private static char[,] GetMap()
         {
-            var xOffset = 465;
-            var map = new char[1896, 655 - xOffset];
+            var xOffset = 460;
+            var yOffset = 5;
+            var map = new char[1896 - yOffset, 700 - xOffset];
             for (int y = 0; y < map.GetLength(0); y++)
             {
                 for (int x = 0; x < map.GetLength(1); x++)
@@ -243,67 +139,30 @@ namespace Day17
                     map[y, x] = '.';
                 }
             }
-            map[0, 500 - xOffset] = '+';
-
             foreach (var line in File.ReadAllLines("input"))
             {
-                var parts = line.Split(", ");
-                if (line.StartsWith("x"))
+                var m = Regex.Match(line, @"(\w)=(\d+), \w=(\d+)..(\d+)");
+                var pos = int.Parse(m.Groups[2].Value);
+                var start = int.Parse(m.Groups[3].Value);
+                var end = int.Parse(m.Groups[4].Value);
+                if (m.Groups[1].Value == "x")
                 {
-                    var x = int.Parse(parts[0].Substring(2)) - xOffset;
-                    var toFrom = parts[1].Substring(2).Split("..").Select(i => int.Parse(i)).ToArray();
-                    for (int y = toFrom[0]; y < toFrom[1]; y++)
+                    for (int i = start; i <= end; i++)
                     {
-                        map[y, x] = '#';
+                        map[i - yOffset, pos - xOffset] = '#';
                     }
                 }
                 else
                 {
-                    var y = int.Parse(parts[0].Substring(2));
-                    var toFrom = parts[1].Substring(2).Split("..").Select(i => int.Parse(i) - xOffset).ToArray();
-                    for (int x = toFrom[0]; x < toFrom[1]; x++)
+                    for (int i = start; i <= end; i++)
                     {
-                        map[y, x] = '#';
+                        map[pos - yOffset, i - xOffset] = '#';
                     }
                 }
             }
 
             return map;
         }
-    }
-
-    public class Bucket
-    {
-
-        public Bucket()
-        {
-            SubBuckets = new List<Bucket>();
-            Dividers = new List<Divider>();
-        }
-
-        public int Width { get; set; }
-
-        public int Height { get; set; }
-
-        public List<Divider> Dividers { get; set; }
-
-        public List<Bucket> SubBuckets { get; set; }
-
-        public int DistanceToParent { get; set; }
-
-    }
-
-    public class Divider
-    {
-
-        public int XStart { get; set; }
-
-        public int YStart { get; set; }
-
-        public int XStop { get; set; }
-
-        public int YStop { get; set; }
-
     }
 
 }
---
+++ 2018/Day25/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.FirstStar();
+        }
+
+        public void FirstStar()
+        {
+            var data = GetData();
+            var set = data.Select(d => new List<int[]> { d.Split(',').Select(i => int.Parse(i)).ToArray() }).ToList();
+            var prevSize = set.Count + 1;
+            while (set.Count < prevSize)
+            {
+                prevSize = set.Count;
+                var removedSet = new HashSet<List<int[]>>();
+                for (int i = 0; i < set.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < set.Count; j++)
+                    {
+                        if (!removedSet.Contains(set[i]) && !removedSet.Contains(set[j]))
+                        {
+                            if (set[i].Any(s => set[j].Any(n => Enumerable.Range(0, 4).Sum(k => Math.Abs(s[k] - n[k])) <= 3)))
+                            {
+                                set[i].AddRange(set[j]);
+                                removedSet.Add(set[j]);
+                            }
+                        }
+                    }
+                }
+                foreach (var removed in removedSet)
+                {
+                    set.Remove(removed);
+                }
+            }
+            System.Console.WriteLine(set.Count);
+        }
+
+        string[] GetData() => File.ReadAllLines("input");
+
+    }
+}
