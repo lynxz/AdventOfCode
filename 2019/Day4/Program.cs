19462f492727145b93828b6cbeb75bf476fb80f9
Anders Strömberg <anders.stroemberg@gmail.com>
2019-12-04 07:20:38 +0100

    Cleaned up a bit.

--- 2019/Day4/Program.cs
+++ 2019/Day4/Program.cs
@@ -17,25 +17,13 @@ namespace Day4
         {
             var start = 284639;
             var stop = 748759;
-            var d = new[] { 1, 10, 100, 1000, 10000, 100_000, 1_000_000 };
             var count = 0;
 
             for (; start < stop; start++)
             {
-                var parts = Enumerable.Range(0, d.Length - 1).Select(i =>  (start / d[i]) % 10 ).ToArray();
-                var correct = true;
-                var anyDouble = false;
-                for (int i = 0; i < parts.Length - 1; i++)
-                {
-                    if (parts[i] < parts[i + 1])
-                    {
-                        correct = false;
-                    }
-                    if (parts[i] == parts[i + 1])
-                    {
-                        anyDouble = true;
-                    }
-                }
+                var parts = start.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+                var anyDouble = Enumerable.Range(0, parts.Length - 1).Any(i => parts[i] == parts[i + 1]);
+                var correct = Enumerable.Range(0, parts.Length - 1).All(i => parts[i] <= parts[i + 1]);
                 if (correct && anyDouble)
                 {
                     count++;
@@ -48,28 +36,13 @@ namespace Day4
         {
             var start = 284639;
             var stop = 748759;
-            var d = new[] { 1, 10, 100, 1000, 10000, 100_000, 1_000_000 };
             var count = 0;
 
             for (; start < stop; start++)
             {
-                var parts = Enumerable.Range(0, d.Length - 1).Select(i =>  (start / d[i]) % 10 ).ToArray();
-                var correct = true;
-                var anyDouble = false;
-                for (int i = 0; i < parts.Length - 1; i++)
-                {
-                    if (parts[i] < parts[i + 1])
-                    {
-                        correct = false;
-                    }
-                    if (parts[i] == parts[i + 1])
-                    {
-                        if ((i + 2 == parts.Length || parts[i+2] != parts[i]) && (i -1 == -1 || parts[i-1] != parts[i])) {
-                            anyDouble = true;
-                        }
-                        
-                    }
-                }
+                var parts = start.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+                var anyDouble = Enumerable.Range(0, parts.Length - 1).Any(i => (parts[i] == parts[i + 1]) && ((i + 2 == parts.Length || parts[i + 2] != parts[i]) && (i - 1 == -1 || parts[i - 1] != parts[i])));
+                var correct = Enumerable.Range(0, parts.Length - 1).All(i => parts[i] <= parts[i + 1]);
                 if (correct && anyDouble)
                 {
                     count++;
@@ -78,8 +51,6 @@ namespace Day4
             System.Console.WriteLine(count);
         }
 
-        IEnumerable<string> GetData()
-            => File.ReadAllLines("input.txt");
 
     }
 }
