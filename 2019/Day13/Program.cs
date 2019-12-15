98a1d28280ec37811b850bf5967b9158979e27e5
Anders Strömberg <anders.stroemberg@gmail.com>
2019-12-15 22:32:17 +0100

    Day 14 complete.

---
+++ 2019/Day13/Program.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.SecondStar();
+        }
+
+        public void FirstStar()
+        {
+            var input = 0;
+            var x = 0;
+            var y = 0;
+            var data = new List<(int x, int y, int tile)>();
+            var ops = GetData();
+            var computer = new IntComputer(ops, () => 1, i =>
+            {
+                switch (input)
+                {
+                    case 0:
+                        x = Convert.ToInt32(i);
+                        break;
+                    case 1:
+                        y = Convert.ToInt32(i);
+                        break;
+                    case 2:
+                        data.Add((x, y, Convert.ToInt32(i)));
+                        break;
+                }
+                input = (input + 1) % 3;
+            });
+            computer.RunProgram();
+            System.Console.WriteLine(data.Count(d => d.tile == 2));
+        }
+
+        public void SecondStar()
+        {
+            var memmory = File.ReadAllLines("mem.txt").Select(i => int.Parse(i)).ToList(); ;
+            var display = new int[37, 20];
+            var score = 0;
+            var input = 0;
+            var x = 0;
+            var y = 0;
+            var ops = GetData();
+            int step = 0;
+            ops[0] = 2;
+            var computer = new IntComputer(ops, () =>
+            {
+                if (step < memmory.Count)
+                {
+                    Console.Write('.');
+                    return memmory[step++];
+                }
+                DrawDisplay(display);
+                step = int.MaxValue;
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    memmory.Add(-1);
+                    return -1;
+                }
+                if (key.Key == ConsoleKey.RightArrow)
+                {
+                    memmory.Add(1);
+                    return 1;
+                }
+                memmory.Add(0);
+                return 0;
+            }
+            , i =>
+             {
+                 switch (input)
+                 {
+                     case 0:
+                         x = Convert.ToInt32(i);
+                         break;
+                     case 1:
+                         y = Convert.ToInt32(i);
+                         break;
+                     case 2:
+                         var tile = Convert.ToInt32(i);
+                         if (x == -1)
+                         {
+                             score = tile;
+                         }
+                         else
+                         {
+                             display[x, y] = tile;
+                         }
+                         break;
+                 }
+                 input = (input + 1) % 3;
+             });
+            computer.RunProgram();
+            using (var fs = new StreamWriter(new FileStream("mem.txt", FileMode.Truncate)))
+            {
+                foreach (var mem in memmory)
+                {
+                    fs.WriteLine(mem);
+                }
+            }
+            System.Console.WriteLine(score);
+        }
+
+        void DrawDisplay(int[,] display)
+        {
+            System.Console.WriteLine();
+            var output = new Dictionary<int, string> { { 0, " " }, { 1, "|" }, { 2, "#" }, { 3, "—" }, { 4, "o" } };
+            for (int y = 0; y < 20; y++)
+            {
+                for (int x = 0; x < 37; x++)
+                {
+                    Console.Write(output[display[x, y]]);
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        List<long> GetData() =>
+            File.ReadAllText("input.txt").Split(",").Select(i => long.Parse(i)).ToList();
+
+    }
+
+    public class IntComputer
+    {
+
+        int _pos = 0;
+        List<long> _ops;
+        Func<long> _getInput;
+        Action<long> _writeOutput;
+        Dictionary<int, long> _heap;
+        int _relative = 0;
+
+        public IntComputer(List<long> ops, Func<long> getInput, Action<long> wrtieOutput)
+        {
+            _ops = ops;
+            _getInput = getInput;
+            _writeOutput = wrtieOutput;
+            _heap = new Dictionary<int, long>();
+        }
+
+        public bool Halted { get; private set; }
+
+        public void RunProgram()
+        {
+            while (true)
+            {
+                var data = Evaluate(_pos, _ops);
+                if (data[0] == 99)
+                {
+                    Halted = true;
+                    System.Console.WriteLine("HALT!");
+                    return;
+                }
+                else if (data[0] == 1)
+                {
+                    Store(data[3], data[1] + data[2]);
+                }
+                else if (data[0] == 2)
+                {
+                    Store(data[3], data[1] * data[2]);
+                }
+                else if (data[0] == 3)
+                {
+                    Store(data[1], _getInput());
+                }
+                else if (data[0] == 4)
+                {
+                    _writeOutput(data[1]);
+                }
+                else if (data[0] == 5)
+                {
+                    if (data[1] != 0)
+                    {
+                        _pos = Convert.ToInt32(data[2]);
+                        data = new long[0];
+                    }
+                }
+                else if (data[0] == 6)
+                {
+                    if (data[1] == 0)
+                    {
+                        _pos = Convert.ToInt32(data[2]);
+                        data = new long[0];
+                    }
+                }
+                else if (data[0] == 7)
+                {
+                    Store(data[3], data[1] < data[2] ? 1u : 0u);
+                }
+                else if (data[0] == 8)
+                {
+                    Store(data[3], data[1] == data[2] ? 1u : 0u);
+                }
+                else if (data[0] == 9)
+                {
+                    _relative += Convert.ToInt32(data[1]);
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+                _pos += data.Length;
+            }
+        }
+
+        void Store(long pos, long value) => Store(Convert.ToInt32(pos), value);
+
+        void Store(int pos, long value)
+        {
+            if (pos >= _ops.Count)
+            {
+                if (!_heap.ContainsKey(pos))
+                {
+                    _heap.Add(pos, 0);
+                }
+                _heap[pos] = value;
+            }
+            else
+            {
+                _ops[pos] = value;
+            }
+        }
+
+        long[] Evaluate(int pos, List<long> ops)
+        {
+            var op = GetData(pos);
+            if (op == 99)
+            {
+                return new[] { op };
+            }
+            else
+            {
+                var code = op % 100;
+                var a = (op / 100) % 10;
+                var b = (op / 1000) % 10;
+                var c = (op / 10000) % 10;
+                if (code == 3)
+                {
+                    return new[] {
+                        code,
+                        GetWriteAddress(pos + 1, a)
+                    };
+                }
+                if (code == 4 || code == 9)
+                {
+                    return new[] {
+                        code,
+                        GetData(pos + 1, a),
+                    };
+                }
+                if (code == 5 || code == 6)
+                {
+                    return new[] {
+                        code,
+                        GetData(pos + 1, a),
+                        GetData(pos + 2, b)
+                    };
+                }
+                return new[] {
+                    code,
+                    GetData(pos + 1, a),
+                    GetData(pos + 2, b),
+                    GetWriteAddress(pos + 3, c)
+                };
+            }
+        }
+
+        long GetWriteAddress(int pos, long mode)
+            => mode == 2 ? GetData(pos) + _relative : GetData(pos);
+
+
+        long GetData(int pos, long mode)
+        {
+            switch (mode)
+            {
+                case 1:
+                    return GetData(pos);
+                case 2:
+                    return GetDataRelative(pos);
+                default:
+                    return GetDataPos(pos);
+            }
+        }
+
+        long GetDataRelative(int pos)
+        {
+            var position = Convert.ToInt32(GetData(pos) + _relative);
+            return GetData(position);
+        }
+
+        long GetDataPos(int pos)
+        {
+            var position = Convert.ToInt32(GetData(pos));
+            return GetData(position);
+        }
+
+        long GetData(int pos)
+        {
+            if (pos >= _ops.Count)
+            {
+                return _heap.ContainsKey(pos) ? _heap[pos] : 0;
+            }
+            return _ops[pos];
+        }
+
+    }
+}
---
+++ 2019/Day14/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.SecondStar();
+        }
+
+        Dictionary<string, List<(int nom, int denom, string name)>> _tree;
+        Dictionary<string, int> _wasteList;
+
+        void FirstStar()
+        {
+            GenerateTree();
+            var minQuantity = GetMinimumOre();
+            System.Console.WriteLine(minQuantity);
+        }
+
+        int GetMinimumOre()
+        {
+            var itemList = new List<(string node, int quantity)> { ("FUEL", 1) };
+
+            while (itemList.Any(n => n.node != "ORE"))
+            {
+                var nodeName = string.Empty;
+                foreach (var node in itemList)
+                {
+                    if (node.node == "ORE")
+                    {
+                        continue;
+                    }
+                    if (!HasOtherDependencies(node.node, itemList.Where(n => n != node)))
+                    {
+                        nodeName = node.node;
+                        break;
+                    }
+                }
+                if (!string.IsNullOrEmpty(nodeName))
+                {
+                    Expand(nodeName, itemList);
+                    itemList = Consolidate(itemList);
+                }
+            }
+
+            return itemList.First().quantity;
+        }
+
+        void SecondStar()
+        {
+            GenerateTree();
+            var minQuantity = GetMinimumOre();
+            var fuel = 1000000000000 / minQuantity;
+            var waste = _wasteList.ToDictionary(k => k.Key, k => _wasteList[k.Key] * fuel);
+            waste.Add("ORE", 1000000000000 - fuel * minQuantity);
+            long oldValue = 0;
+            while (oldValue != fuel)
+            {
+                oldValue = fuel;
+                MergeDown("FUEL", waste);
+                var mult = waste["ORE"] / minQuantity;
+                foreach (var w in _wasteList)
+                {
+                    waste[w.Key] += w.Value * mult;
+                }
+                waste["ORE"] -= mult * minQuantity;
+                fuel += mult;
+            }
+            System.Console.WriteLine(fuel);
+            while(PullUp("FUEL", 1, waste)) {
+                fuel++;
+            }
+            System.Console.WriteLine(fuel);
+        }
+
+        bool PullUp(string currentNode, long quantity, Dictionary<string, long> waste) {
+            if (waste[currentNode] > quantity) {
+                waste[currentNode] -= quantity;
+                return true;
+            };
+            if (currentNode == "ORE") {
+                return false;
+            }
+            var children = _tree[currentNode];
+            var denom = children.First().denom;
+            var required = quantity - waste[currentNode];
+            var mult = required / denom;
+            if (required % denom != 0) {
+                mult++;
+            }
+            waste[currentNode] = mult*denom - required;
+            return children.All(child => PullUp(child.name, child.nom * mult, waste));
+        }
+
+        void MergeDown(string currentNode, Dictionary<string, long> waste)
+        {
+            if (currentNode == "ORE")
+            {
+                return;
+            }
+            var children = _tree[currentNode];
+            var denom = children.First().denom;
+            var multiplier = waste[currentNode] / denom;
+            waste[currentNode] -= multiplier * denom;
+            foreach (var child in children)
+            {
+                waste[child.name] += multiplier * child.nom;
+            }
+            for (int i = children.Count - 1; i > -1; i-- )
+            {
+                MergeDown(children[i].name, waste);
+            }
+        }
+
+        private void GenerateTree()
+        {
+            var regEx = new Regex(@"(\d+)\s([A-Z]+)");
+            _tree = new Dictionary<string, List<(int nom, int denom, string name)>>();
+            foreach (var line in GetData())
+            {
+                var parts = line.Split("=>");
+                var match = regEx.Match(parts[1]);
+                var denom = int.Parse(match.Groups[1].Value);
+                var key = match.Groups[2].Value;
+                var value = regEx.Matches(parts[0]).Select(m => (nom: int.Parse(m.Groups[1].Value), denom: denom, name: m.Groups[2].Value)).ToList();
+                _tree.Add(key, value);
+            }
+            _wasteList = _tree.Keys.ToDictionary(k => k, k => 0);
+        }
+
+        bool HasOtherDependencies(string nodeName, IEnumerable<(string node, int quantity)> subNodes)
+        {
+            if (subNodes.Any(s => s.node == nodeName))
+            {
+                return true;
+            }
+            return subNodes
+                .Where(s => _tree.ContainsKey(s.node))
+                .Any(s => HasOtherDependencies(nodeName, _tree[s.node].Select(n => (n.name, n.nom))));
+        }
+
+        void Expand(string nodeName, List<(string node, int quantity)> itemList)
+        {
+            var node = itemList.First(n => n.node == nodeName);
+            itemList.Remove(node);
+            var treeNode = _tree[nodeName];
+            var denom = treeNode.First().denom;
+            var multiplier = node.quantity / denom;
+            if (node.quantity % denom != 0)
+            {
+                multiplier++;
+            }
+            _wasteList[nodeName] += multiplier * denom - node.quantity;
+            itemList.AddRange(treeNode.Select(n => (n.name, n.nom * multiplier)));
+        }
+
+        List<(string node, int quantity)> Consolidate(List<(string node, int quantity)> itemList) =>
+             itemList.GroupBy(n => n.node).Select(g => (node: g.Key, quantity: g.Sum(n => n.quantity))).ToList();
+
+
+        string[] GetData() =>
+            File.ReadAllLines("input.txt");
+
+    }
+}
