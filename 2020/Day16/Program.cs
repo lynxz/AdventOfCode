0f64e0e4fc87ae8f8391e98ed301c4308ebd8989
Anders Strömberg <anders.stroemberg@gmail.com>
2020-12-16 08:33:43 +0100

    Day16.1

--- 2020/Day16/Program.cs
+++ 2020/Day16/Program.cs
@@ -17,71 +17,19 @@ namespace Day16
 
         void FirstStar()
         {
-            var data = GetData();
-            var rules = new List<Rule>();
-            var index = 0;
-            for (; index < data.Count; index++)
-            {
-                if (data[index] == string.Empty)
-                {
-                    break;
-                }
-                var name = data[index].Split(':')[0];
-                var ranges = GetInts(data[index]);
-                rules.Add(new Rule(name, new Range(ranges[0], ranges[1]), new Range(ranges[2], ranges[3])));
-            }
-            index += 2;
-            var myTicket = GetInts(data[index]);
-
-            index += 3;
-            var otherTickets = new List<List<int>>();
-            for (; index < data.Count; index++)
-            {
-                otherTickets.Add(GetInts(data[index]));
-            }
-
-            var scanningErrorRate = 0;
-            foreach (var ticket in otherTickets)
-            {
-                var errors = ticket.Where(v => !rules.Any(r => (v >= r.FirstRange.Start && v <= r.FirstRange.End) || (v >= r.SecondRange.Start && v <= r.SecondRange.End)));
-                scanningErrorRate += errors.Sum();
-            }
+            var (rules, myTicket, otherTickets) = GetData();
+            var scanningErrorRate = otherTickets.Sum(t => t.Where(v => !rules.Any(r => r.Valid(v))).Sum());
             System.Console.WriteLine(scanningErrorRate);
         }
 
         void SecondStar()
         {
-            var data = GetData();
-            var rules = new List<Rule>();
-            var index = 0;
-            for (; index < data.Count; index++)
-            {
-                if (data[index] == string.Empty)
-                {
-                    break;
-                }
-                var name = data[index].Split(':')[0];
-                var ranges = GetInts(data[index]);
-                rules.Add(new Rule(name, new Range(ranges[0], ranges[1]), new Range(ranges[2], ranges[3])));
-            }
-            index += 2;
-            var myTicket = GetInts(data[index]);
+            var (rules, myTicket, otherTickets) = GetData();
 
-            index += 3;
-            var otherTickets = new List<List<int>>();
-            for (; index < data.Count; index++)
-            {
-                otherTickets.Add(GetInts(data[index]));
-            }
-
-            var matchings = new List<List<(int index, List<Rule> rules)>>();
-            foreach (var ticket in otherTickets)
-            {
-                if (!ticket.Any(v => !rules.Any(r => (v >= r.FirstRange.Start && v <= r.FirstRange.End) || (v >= r.SecondRange.Start && v <= r.SecondRange.End))))
-                {
-                    matchings.Add(ticket.Select(v => (ticket.IndexOf(v), rules.Where(r => (v >= r.FirstRange.Start && v <= r.FirstRange.End) || (v >= r.SecondRange.Start && v <= r.SecondRange.End)).ToList())).ToList());
-                }
-            }
+            // May any deity forgive me for this monstrosity
+            List<List<(int index, List<Rule> rules)>> matchings = otherTickets
+                .Where(t => !t.Any(v => !rules.Any(r => r.Valid(v))))
+                .Select(t => t.Select(v => (t.IndexOf(v), rules.Where(r => r.Valid(v)).ToList())).ToList()).ToList();
 
 
             var fields = new List<(int index, Rule rule)>();
@@ -104,14 +52,49 @@ namespace Day16
             System.Console.WriteLine(result);
         }
 
-        List<int> GetInts(string line) => Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value)).ToList();
+        (List<Rule> Rules, List<int> MyTicket, List<List<int>> OtherTickets) GetData()
+        {
+            var data = File.ReadAllLines("data.txt").ToList();
+            var rules = new List<Rule>();
+            var index = 0;
+            for (; index < data.Count; index++)
+            {
+                if (data[index] == string.Empty)
+                {
+                    break;
+                }
+                var name = data[index].Split(':')[0];
+                var ranges = GetInts(data[index]);
+                rules.Add(new Rule(name, new Range(ranges[0], ranges[1]), new Range(ranges[2], ranges[3])));
+            }
+            index += 2;
+            var myTicket = GetInts(data[index]);
 
-        List<string> GetData() => File.ReadAllLines("data.txt").ToList();
+            index += 3;
+            var otherTickets = new List<List<int>>();
+            for (; index < data.Count; index++)
+            {
+                otherTickets.Add(GetInts(data[index]));
+            }
+            return (rules, myTicket, otherTickets);
+        }
+
+        List<int> GetInts(string line) => Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value)).ToList();
 
     }
 
-    record Rule(string Name, Range FirstRange, Range SecondRange);
+    record Rule(string Name, Range FirstRange, Range SecondRange)
+    {
 
-    record Range(int Start, int End);
+        public bool Valid(int value) => FirstRange.InRange(value) || SecondRange.InRange(value);
+
+    }
+
+    record Range(int Start, int End)
+    {
+
+        public bool InRange(int value) => value >= Start && value <= End;
+
+    }
 
 }
