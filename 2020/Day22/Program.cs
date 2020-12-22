8cebca28bab8e55d2d35acad5806f2760eb1e143
Anders Strömberg <anders.stroemberg@gmail.com>
2020-12-22 15:31:58 +0100

    Day22.1

--- 2020/Day22/Program.cs
+++ 2020/Day22/Program.cs
@@ -16,87 +16,53 @@ namespace Day22
 
         private void FirstStar()
         {
-            var data = GetData();
-            Queue<int> player1 = new();
-            Queue<int> player2 = new();
-            var split = data.IndexOf(string.Empty);
-            data.Skip(1).Take(split - 1).Select(n => int.Parse(n)).ToList().ForEach(n => player1.Enqueue(n));
-            data.Skip(split + 2).Take(data.Count - split - 2).Select(n => int.Parse(n)).ToList().ForEach(n => player2.Enqueue(n));
-            while (player1.Count > 0 && player2.Count > 0)
+            var players = GetData();
+            while (players.All(p => p.Count > 0))
             {
-                var p1 = player1.Dequeue();
-                var p2 = player2.Dequeue();
-                if (p1 > p2)
-                {
-                    player1.Enqueue(p1);
-                    player1.Enqueue(p2);
-                }
-                else
-                {
-                    player2.Enqueue(p2);
-                    player2.Enqueue(p1);
-                }
+                var cards = players.Select(p => p.Dequeue()).ToArray();
+                var winner = cards[0] > cards[1] ? 0 : 1;
+                players[winner].Enqueue(cards[winner]);
+                players[winner].Enqueue(cards[(winner + 1) % 2]);
             }
-            var winner = player1.Count > player2.Count ? player1 : player2;
-            var result = Enumerable.Range(1, winner.Count).Reverse().Sum(i => i * winner.Dequeue());
-            System.Console.WriteLine(result);
+            System.Console.WriteLine(players[players[0].Count > players[1].Count ? 0 : 1].Reverse().Select((c, i) => c * (i + 1)).Sum());
         }
 
         private void SecondStar()
         {
-            var data = GetData();
-            Queue<int> player1 = new();
-            Queue<int> player2 = new();
-
-            var split = data.IndexOf(string.Empty);
-            data.Skip(1).Take(split - 1).Select(n => int.Parse(n)).ToList().ForEach(n => player1.Enqueue(n));
-            data.Skip(split + 2).Take(data.Count - split - 2).Select(n => int.Parse(n)).ToList().ForEach(n => player2.Enqueue(n));
-            var winner = Play(player1, player2) == 1 ? player1 : player2;
-            System.Console.WriteLine(Enumerable.Range(1, winner.Count).Reverse().Sum(i => i * winner.Dequeue()));
+            var players = GetData();
+            var winner = Play(players);
+            System.Console.WriteLine(Enumerable.Range(1, players[winner].Count).Reverse().Sum(i => i * players[winner].Dequeue()));
         }
 
-        private static int Play(Queue<int> player1, Queue<int> player2)
+        private static int Play(Queue<int>[] players)
         {
-            HashSet<string> mem1 = new();
-            HashSet<string> mem2 = new();
-            while (player1.Count > 0 && player2.Count > 0)
+            var memories = new [ ] {new HashSet<string>(), new HashSet<string>()};
+            while (players.All(p => p.Count > 0))
             {
-                var hash1 = string.Join(" ", player1.Select(i => i.ToString()));
-                var hash2 = string.Join(" ", player2.Select(i => i.ToString()));
-                if (mem1.Contains(hash1) || mem2.Contains(hash2))
-                {
-                    return 1;
-                }
-                mem1.Add(hash1);
-                mem2.Add(hash2);
+                var hashes = players.Select(p => string.Join(" ", p.Select(i => i))).ToArray();
+                if (Enumerable.Range(0,2).Any(i => memories[i].Contains(hashes[i])))
+                    return 0;
+                
+                Enumerable.Range(0,2).ToList().ForEach(i => memories[i].Add(hashes[i]));
 
-                var p1 = player1.Dequeue();
-                var p2 = player2.Dequeue();
+                var cards = players.Select(p => p.Dequeue()).ToArray();
                 var winner = 0;
-                if (player1.Count >= p1 && player2.Count >= p2)
-                {
-                    winner = Play(new Queue<int>(player1.Take(p1)), new Queue<int>(player2.Take(p2)));
-                }
+                if (Enumerable.Range(0, 2).All(i => players[i].Count >= cards[i]))
+                    winner = Play(players.Select((p, i) => new Queue<int>(p.Take(cards[i]))).ToArray());
                 else
-                {
-                    winner = p1 > p2 ? 1 : 2;
-                }
-                if (winner == 1)
-                {
-                    player1.Enqueue(p1);
-                    player1.Enqueue(p2);
-                }
-                else
-                {
-                    player2.Enqueue(p2);
-                    player2.Enqueue(p1);
-                }
+                    winner = cards[0] > cards[1] ? 0 : 1;
+
+                players[winner].Enqueue(cards[winner]);
+                players[winner].Enqueue(cards[(winner + 1) % 2]);
             }
 
-            return player1.Count > player2.Count ? 1 : 2;
+            return players[0].Count > players[1].Count ? 0 : 1;
         }
 
-        List<string> GetData() => File.ReadAllLines("data.txt").ToList();
+        Queue<int>[] GetData() => File.ReadAllText("data.txt")
+            .Split("\r\n\r\n")
+            .Select(p => new Queue<int>(p.Split(Environment.NewLine).Skip(1).Select(int.Parse)))
+            .ToArray();
 
     }
 }
