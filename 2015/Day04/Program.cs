8a0698f9378bf490065bff1d072ab3cbd628895f
lynxz <anders.stroemberg@gmail.com>
2021-11-28 22:11:04 +0100

    Adding Day 4 and Day 5

---
+++ 2015/Day04/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using Tools;
+
+var day = new Day4();
+day.PostSecondStar();
+
+public class Day4 : DayBase
+{
+    public Day4() : base("4")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var input = "bgvyzdsv";
+
+        for (int i = 0; i < int.MaxValue; i++) {
+            var data = $"{input}{i}";
+            var hash = CreateMD5(data);
+            if (hash.Take(5).All(c => c == '0')) 
+                return i.ToString();
+        }
+        return "-1";
+    }
+
+    public override string SecondStar()
+    {
+       var input = "bgvyzdsv";
+
+        for (int i = 0; i < int.MaxValue; i++) {
+            var data = $"{input}{i}";
+            var hash = CreateMD5(data);
+            if (hash.Take(6).All(c => c == '0')) 
+                return i.ToString();
+        }
+        return "-1";
+    }
+
+    public static string CreateMD5(string input)
+    {
+        // Use input string to calculate MD5 hash
+        using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 2015/Day05/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tools;
+
+var day = new Day5();
+day.PostSecondStar();
+
+public class Day5 : DayBase
+{
+    public Day5() : base("5")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRowData();
+        var vowels = "aeiou".ToArray();
+        var badStrings = "ab cd pq xy".Split(' ');
+        return data.Count(l =>
+            l.Count(d => vowels.Any(v => v == d)) > 2 &&
+            Enumerable.Range(0, l.Length - 1).Any(i => l[i] == l[i + 1]) &&
+            !badStrings.Any(bs => l.Contains(bs))
+            ).ToString();
+    }
+
+    public override string SecondStar()
+    {
+        var data = GetRowData();
+        return data.Count(l =>
+            Enumerable.Range(0, l.Length - 2).Any(i => l[i] == l[i + 2]) &&
+            Enumerable.Range(0, l.Length - 1).Where(i => l[i] == l[i+1] ? i + 2 < l.Length - 1 ? l[i] != l[i+2] : true : true).Select(i => (new string(l.Skip(i).Take(2).ToArray()), 1)).GroupBy(x => x.Item1).Any(g => g.Sum(x => x.Item2) > 1)
+        ).ToString();
+    }
+}
