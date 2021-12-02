fb535ceb2d9c3480d8c442e3d64596f9ccc0c3bd
lynxz <anders.stroemberg@gmail.com>
2021-12-02 06:18:24 +0100

    Day 2 2021 completed

--- 2021/Day02/Program.cs
+++ 2021/Day02/Program.cs
@@ -2,7 +2,7 @@
 using Tools;
 
 var day = new Day2("2");
-day.PostFirstStar();
+day.PostSecondStar();
 
 public class Day2 : DayBase
 {
@@ -12,11 +12,42 @@ public class Day2 : DayBase
 
     public override string FirstStar()
     {
-        throw new NotImplementedException();
+        var input = GetRowData().Select(d => d.Split(' ')).Select(p => (Command: p[0], Step: int.Parse(p[1]))).ToList();
+        var depth = 0;
+        var pos = 0;
+        foreach (var i in input)
+        {
+            if (i.Command == "forward")
+                pos += i.Step;
+            else if (i.Command == "up")
+                depth -= i.Step;
+            else
+                depth += i.Step;
+        }
+
+        return (depth * pos).ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var input = GetRowData().Select(d => d.Split(' ')).Select(p => (Command: p[0], Step: int.Parse(p[1]))).ToList();
+        var depth = 0;
+        var pos = 0;
+        var aim = 0;
+        foreach (var i in input)
+        {
+            if (i.Command == "forward")
+            {
+                pos += i.Step;
+                depth += aim * i.Step;
+            }
+
+            else if (i.Command == "up")
+                aim -= i.Step;
+            else
+                aim += i.Step;
+        }
+
+        return (depth * pos).ToString();
     }
 }
---
+++ 2021/Day03/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tools;
+
+Console.WriteLine("Hello, World!");
+
+var day = new Day3("3");
+day.PostFirstStar();
+
+public class Day3 : DayBase
+{
+    public Day3(string day) : base(day)
+    {
+    }
+
+    public override string FirstStar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
