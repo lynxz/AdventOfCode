39763631b1baecefa2b3b4b5b332ff1cc0ccd065
lynxz <anders.stroemberg@gmail.com>
2021-12-06 07:26:28 +0100

    Day 6 2021

--- 2021/Day06/Program.cs
+++ 2021/Day06/Program.cs
@@ -2,7 +2,7 @@
 using Tools;
 
 var day = new Day6("6");
-day.PostFirstStar();
+day.OutputSecondStar();
 
 public class Day6 : DayBase
 {
@@ -12,11 +12,50 @@ public class Day6 : DayBase
 
     public override string FirstStar()
     {
-        throw new NotImplementedException();
+        var input = GetRawData().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToList();
+        for (int i = 0; i < 80; i++)
+        {
+            var k = input.Count;
+            for (int j = 0; j < k; j++)
+            {
+                input[j]--;
+                if (input[j] == -1)
+                {
+                    input[j] = 6;
+                    input.Add(8);
+                }
+            }
+        }
+        return input.Count.ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var input = GetRawData().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => long.Parse(i)).ToList();
+        var l = input.Count;
+        var fish = new long[256];
+        for (int i = 0; i < 9; i++)
+        {
+            var k = input.Count;
+            for (int j = 0; j < k; j++)
+            {
+                input[j]--;
+                if (input[j] == -1)
+                {
+                    input[j] = 6;
+                    input.Add(8);
+                }
+            }
+            fish[i] = input.Count - k;
+        }
+
+        for (int i = 9; i < 256; i++)
+        {
+            fish[i] = fish[i - 7] + fish[i - 9];
+        }
+
+        var d = fish.Sum() + l;
+
+        return (fish.Sum() + l).ToString();
     }
 }
---
+++ 2021/Day07/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tools;
+
+var day = new Day7("7");
+day.PostFirstStar();
+
+public class Day7 : DayBase
+{
+    public Day7(string day) : base(day)
+    {
+    }
+
+    public override string FirstStar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
