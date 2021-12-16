958b819fe83e6ce0f0fc766f951ce2348e372deb
lynxz <anders.stroemberg@gmail.com>
2021-12-16 07:41:06 +0100

    Day 16 2021

--- 2021/Day16/Program.cs
+++ 2021/Day16/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 using Tools;
 
 Day16 day = new("16");
-day.OutputFirstStar();
+day.OutputSecondStar();
 
 public class Day16 : DayBase
 {
@@ -12,11 +13,126 @@ public class Day16 : DayBase
 
     public override string FirstStar()
     {
-        throw new NotImplementedException();
+        var binString = GetBinaryString();
+        var index = 0;
+        var package = ParsePackage(binString, ref index);
+        return package.GetVersionValue().ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var binString = GetBinaryString();
+        var index = 0;
+        var package = ParsePackage(binString, ref index);
+        return package.GetValue().ToString();
     }
+
+    private string GetBinaryString()
+    {
+        var hex = new Dictionary<char, string>{
+            {'0', "0000"},
+            {'1', "0001"},
+            {'2', "0010"},
+            {'3', "0011"},
+            {'4', "0100"},
+            {'5', "0101"},
+            {'6', "0110"},
+            {'7', "0111"},
+            {'8', "1000"},
+            {'9', "1001"},
+            {'A', "1010"},
+            {'B', "1011"},
+            {'C', "1100"},
+            {'D', "1101"},
+            {'E', "1110"},
+            {'F', "1111"}
+        };
+        var input = GetRawData().Trim();
+        return string.Join("", input.Select(c => hex[c]));
+    }
+
+    private static Package ParsePackage(string binString, ref int index)
+    {
+        var package = new Package
+        {
+            Version = Convert.ToInt32(binString.Substring(index, 3), 2),
+            TypeId = Convert.ToInt32(binString.Substring(index + 3, 3), 2)
+        };
+        index += 6;
+        switch (package.TypeId)
+        {
+            case 4:
+                var valString = string.Empty;
+                StringBuilder sb = new();
+                do
+                {
+                    valString = binString.Substring(index, 5);
+                    index += 5;
+                    sb.Append(valString.Substring(1));
+                } while (valString.First() == '1');
+                package.Value = Convert.ToInt64(sb.ToString(), 2);
+                return package;
+            default:
+                if (binString[index++] == '0')
+                {
+                    var len = Convert.ToInt32(binString.Substring(index, 15), 2);
+                    index += 15;
+                    var subPackages = binString.Substring(index, len);
+                    index += len;
+                    int newIndexLen = 0;
+                    while (newIndexLen < len)
+                        package.SubPackages.Add(ParsePackage(subPackages, ref newIndexLen));
+                }
+                else
+                {
+                    var noOfPackages = Convert.ToInt32(binString.Substring(index, 11), 2);
+                    index += 11;
+                    for (int i = 0; i < noOfPackages; i++)
+                        package.SubPackages.Add(ParsePackage(binString, ref index));
+                }
+                return package;
+        }
+
+        throw new Exception();
+    }
+}
+
+public class Package
+{
+
+    public int Version { get; set; }
+
+    public int TypeId { get; set; }
+
+    public long Value { get; set; }
+
+    public List<Package> SubPackages { get; } = new List<Package>();
+
+    public int GetVersionValue() => Version + SubPackages.Sum(p => p.GetVersionValue());
+
+    public long GetValue()
+    {
+        switch (TypeId)
+        {
+            case 0:
+                return SubPackages.Sum(p => p.GetValue());
+            case 1:
+                return SubPackages.Aggregate(1L, (acc, p) => acc * p.GetValue());
+            case 2:
+                return SubPackages.Min(p => p.GetValue());
+            case 3:
+                return SubPackages.Max(p => p.GetValue());
+            case 4:
+                return Value;
+            case 5:
+                return SubPackages[0].GetValue() > SubPackages[1].GetValue() ? 1L : 0L;
+            case 6:
+                return SubPackages[0].GetValue() < SubPackages[1].GetValue() ? 1L : 0L;
+            case 7:
+                return SubPackages[0].GetValue() == SubPackages[1].GetValue() ? 1L : 0L;
+        }
+
+        throw new Exception();
+    }
+
 }
---
+++ 2021/Day17/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tools;
+
+Day17 day = new("17");
+day.PostFirstStar();
+
+public class Day17 : DayBase
+{
+    public Day17(string day) : base(day)
+    {
+    }
+
+    public override string FirstStar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
