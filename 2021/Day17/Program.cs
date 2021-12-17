5782726a213643200c759121ca64bec908c2124f
lynxz <anders.stroemberg@gmail.com>
2021-12-17 08:05:41 +0100

    Day 17 2021

--- 2021/Day17/Program.cs
+++ 2021/Day17/Program.cs
@@ -2,7 +2,7 @@
 using Tools;
 
 Day17 day = new("17");
-day.PostFirstStar();
+day.OutputSecondStar();
 
 public class Day17 : DayBase
 {
@@ -12,11 +12,39 @@ public class Day17 : DayBase
 
     public override string FirstStar()
     {
-        throw new NotImplementedException();
+        return GeoSum(139).ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var xMax = 164;
+        var xMin = 117;
+        var yMin = -140;
+        var yMax = -89;
+        var xPos = Enumerable.Range(1, xMax).Where(i => Enumerable.Range(1, i).Any(j => GeoSum(i) - GeoSum(j) >= xMin && GeoSum(i) - GeoSum(j) <= xMax)).ToList();
+        var pairs = xPos.SelectMany(x => Enumerable.Range(yMin, 2 * (-yMin)).Where(y =>
+        {
+            var vx = x;
+            var vy = y;
+            var px = 0;
+            var py = 0;
+            while (px <= xMax && py >= yMin)
+            {
+                px += vx;
+                py += vy;
+                vx = Math.Max(0, vx - 1);
+                vy -= 1;
+                if (px >= xMin && px <= xMax && py >= yMin && py <= yMax)
+                    return true;
+            }
+
+            return false;
+        }).Select(y => (Y: y, X: x))).ToList();
+
+        return pairs.Count.ToString();
     }
+
+    int GeoSum(int i) => i * (i + 1) / 2;
+
+
 }
---
+++ 2021/Day18/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tools;
+
+Day18 day = new("18");
+day.OutputFirstStar();
+
+public class Day18 : DayBase
+{
+    public Day18(string day) : base(day)
+    {
+    }
+
+    public override string FirstStar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
