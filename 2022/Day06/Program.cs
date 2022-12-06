73a877d219905bc0668c17e322dfd1240b0fefc3
anders.stromberg <anders.stroemberg@gmail.com>
2022-12-06 06:18:12 +0100

    Day6

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tools;
+
+Day06 day = new();
+day.PostSecondStar();
+
+public class Day06 : DayBase
+{
+    public Day06() : base("6")
+    {
+    }
+
+    public override string FirstStar()=>
+        GetPacket(4).ToString();
+
+    public override string SecondStar() =>
+        GetPacket(14).ToString();
+    
+    private int GetPacket(int size)
+    {
+        var data = GetRawData().Window(size).ToList();
+        return Enumerable.Range(0, data.Count).First(i => data[i].Distinct().Count() == size) + size;
+    }
+}
