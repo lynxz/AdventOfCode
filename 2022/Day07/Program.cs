a0a7c95461391711802911b0a89ba16925518ff2
anders.stromberg <anders.stroemberg@gmail.com>
2022-12-07 07:45:49 +0100

    Cleanup

--- 2022/Day07/Program.cs
+++ 2022/Day07/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Tools;
 
 Day07 day = new();
-day.PostSecondStar();
+day.OutputFirstStar();
+day.OutputSecondStar();
 
 public class Day07 : DayBase
 {
@@ -10,144 +11,95 @@ public class Day07 : DayBase
     }
     public override string FirstStar()
     {
-        var data = GetRowData();
-        var root = new Node("/", 0, null, new List<Node>());
-        var current = root;
-        var c = 0;
-
-        foreach (var l in data.Skip(1))
-        {
-            if (l == "$ cd ..")
-            {
-                current = current.Parent;
-            }
-            else if (l.StartsWith("$ cd"))
-            {
-                var dir = l.Substring(5);
-                Node node = current.Children.FirstOrDefault(n => n.Name == dir);
-                if (node == null)
-                {
-                    node = new Node(dir, 0, current, new List<Node>());
-                    current.Children.Add(node);
-                }
-                current = node;
-            }
-            else if (l.StartsWith("dir"))
-            {
-                var dir = l.Substring(4);
-                Node node = current.Children.FirstOrDefault(n => n.Name == dir);
-                if (node == null)
-                {
-                    node = new Node(dir, 0, current, new List<Node>());
-                    current.Children.Add(node);
-                }
-            }
-            else if (char.IsNumber(l[0]))
-            {
-                var parts = l.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                var node = new Node(parts[1], int.Parse(parts[0]), current, null);
-                current.Children.Add(node);
-            }
-            c++;
-        }
-
-        int result = 0;
-        CalculateDirSize(root, ref result);
-
-        return result.ToString();
-    }
-
-
-    private void CalculateDirSize(Node node, ref int current)
-    {
-        if (node.Size > 0)
-            return;
-        var size = 0;
-        foreach (var child in node.Children)
-        {
-            if (child.Children != null)
-            {
-                CalculateDirSize(child, ref current);
-            }
-            size += child.Size;
-        }
-        node.Size = size;
-        if (size <= 100000) 
-            current += size;
+        Node root = BuildDirectory();
+        return SumSmallDir(root, 0).ToString();
     }
 
     public override string SecondStar()
     {
-        var data = GetRowData();
+        var root = BuildDirectory();
+        var limit = 30000000 - (70000000 - root.Size);
+        return FindDir(root, limit, root).Size.ToString();
+    }
+
+    private Node BuildDirectory()
+    {
+        var data = GetRowData() ?? Array.Empty<string>();
         var root = new Node("/", 0, null, new List<Node>());
         var current = root;
-        var c = 0;
 
         foreach (var l in data.Skip(1))
         {
             if (l == "$ cd ..")
             {
-                current = current.Parent;
+                current = current?.Parent;
             }
             else if (l.StartsWith("$ cd"))
             {
                 var dir = l.Substring(5);
-                Node node = current.Children.FirstOrDefault(n => n.Name == dir);
-                if (node == null)
-                {
-                    node = new Node(dir, 0, current, new List<Node>());
-                    current.Children.Add(node);
-                }
-                current = node;
+                current = current?.Children?.FirstOrDefault(n => n.Name == dir);
             }
             else if (l.StartsWith("dir"))
             {
                 var dir = l.Substring(4);
-                Node node = current.Children.FirstOrDefault(n => n.Name == dir);
+                Node? node = current?.Children?.FirstOrDefault(n => n.Name == dir);
                 if (node == null)
                 {
                     node = new Node(dir, 0, current, new List<Node>());
-                    current.Children.Add(node);
+                    current?.Children?.Add(node);
                 }
             }
             else if (char.IsNumber(l[0]))
             {
                 var parts = l.Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 var node = new Node(parts[1], int.Parse(parts[0]), current, null);
-                current.Children.Add(node);
+                current?.Children?.Add(node);
             }
-            c++;
         }
 
-        CalculateDirSize2(root);
-        var limit = 30000000 - (70000000 - root.Size);
-        var minNode = FindDir(root, limit, root);
-        return minNode.Size.ToString();
+        CalculateDirSize(root);
+
+        return root;
     }
 
-    private void CalculateDirSize2(Node node)
+    private void CalculateDirSize(Node? node)
     {
-        if (node.Size > 0)
+        if (node?.Children == null)
             return;
         var size = 0;
         foreach (var child in node.Children)
         {
             if (child.Children != null)
             {
-                CalculateDirSize2(child);
+                CalculateDirSize(child);
             }
             size += child.Size;
         }
         node.Size = size;
     }
 
-    private Node FindDir(Node node, int limit, Node contender) {
-        if (node.Children == null) {
+    private int SumSmallDir(Node node, int current)
+    {
+        if (node?.Children == null)
+            return current;
+
+        foreach (var child in node.Children)
+            current = SumSmallDir(child, current);
+
+        return node.Size <= 100000 ? node.Size + current : current;
+    }
+
+
+    private Node FindDir(Node node, int limit, Node contender)
+    {
+        if (node.Children == null)
+        {
             return contender;
         }
         if (node.Size >= limit && node.Size < contender.Size)
             contender = node;
-        foreach(var child in node.Children) {
+        foreach (var child in node.Children)
+        {
             contender = FindDir(child, limit, contender);
         }
         return contender;
@@ -156,7 +108,7 @@ public class Day07 : DayBase
 
 public class Node
 {
-    public Node(string name, int size, Node parent, List<Node> children)
+    public Node(string name, int size, Node? parent, List<Node>? children)
     {
         Name = name;
         Size = size;
@@ -166,6 +118,6 @@ public class Node
 
     public string Name { get; set; }
     public int Size { get; set; }
-    public Node Parent { get; set; }
-    public List<Node> Children { get; set; }
+    public Node? Parent { get; set; }
+    public List<Node>? Children { get; set; }
 }
