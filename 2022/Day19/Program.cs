4a9b9e50350160de6940e83e0a2d64e045c5f9d2
anders.stromberg <anders.stroemberg@gmail.com>
2022-12-20 20:37:31 +0100

    Day20

---
+++ 2022/Day19/Program.cs
@@ -0,0 +1,154 @@
+﻿using Tools;
+
+Day19 day = new();
+day.OutputFirstStar();
+day.OutputSecondStar();
+
+public class Day19 : DayBase
+{
+
+    public Day19() : base("19")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRowData();
+        var blueprints = data.Select(l => l.GetIntegers().Skip(1).ToArray()).ToList();
+        var result = Enumerable.Range(1, blueprints.Count).AsParallel()
+            .Select(i => i * MaxProduceGeode(blueprints[i -1], 24)).Sum();
+
+        return result.ToString();
+    }
+
+    public override string SecondStar()
+    {
+        var data = GetRowData();
+        var blueprints = data.Select(l => l.GetIntegers().Skip(1).ToArray()).ToList();
+        var result = Enumerable.Range(1, 3).AsParallel()
+            .Select(i => MaxProduceGeode(blueprints[i -1], 32)).Aggregate((v,i) => v*i);
+        return result.ToString();
+    }
+
+    delegate void ActionRef1<T1, T2>(ref T1 arg1, T2 arg2);
+
+    int MaxProduceGeode(int[] blueprint, int duration)
+    {
+        HashSet<((int ore, int clay, int obsidian, int geodes) resources, (int oreBot, int clayBot, int obsidianBot, int geodeBot) botCounts)> allStates = new() {
+                ((0, 0, 0, 0), (1, 0, 0, 0))
+            };
+        HashSet<((int ore, int clay, int obsidian, int geodes) resources, (int oreBot, int clayBot, int obsidianBot, int geodeBot) botCounts)> nextStates = new() { };
+        List<ActionRef1<((int, int, int, int), (int, int, int, int)), int[]>> buildableBots = new();
+        int bestGeodesSoFar = 0;
+        int bestGeoBotsSoFar = 0;
+        int bestBotsSoFar = 0;
+        int maxOreCost = Math.Max(blueprint[0], Math.Max(blueprint[1], Math.Max(blueprint[2], blueprint[4])));
+
+        for (int time = 1; time <= duration; time++)
+        {
+            foreach (var state in allStates.ToList())
+            {
+                buildableBots.Clear();
+
+                if (state.resources.ore >= blueprint[4] && state.resources.obsidian >= blueprint[5])
+                {
+                    buildableBots.Add(BuildGeodeBot);
+                }
+                if (
+                    state.resources.ore >= blueprint[2] &&
+                    state.resources.clay >= blueprint[3] &&
+                    state.botCounts.obsidianBot < blueprint[5]
+                )
+                {
+                    buildableBots.Add(BuildObsidianBot);
+                }
+                if (state.resources.ore >= blueprint[1] && state.botCounts.oreBot < blueprint[3])
+                {
+                    buildableBots.Add(BuildClayBot);
+                }
+                if (state.resources.ore >= blueprint[0] && state.botCounts.oreBot < maxOreCost)
+                {
+                    buildableBots.Add(BuildOreBot);
+                }
+
+                int currStateBots = state.botCounts.clayBot + state.botCounts.obsidianBot + state.botCounts.geodeBot;
+                if (bestBotsSoFar <= currStateBots || bestBotsSoFar - currStateBots < 10)
+                {
+                    nextStates.Add(((
+                        state.resources.ore + state.botCounts.oreBot,
+                        state.resources.clay + state.botCounts.clayBot,
+                        state.resources.obsidian + state.botCounts.obsidianBot,
+                        state.resources.geodes + state.botCounts.geodeBot
+                    ), (
+                        state.botCounts.oreBot,
+                        state.botCounts.clayBot,
+                        state.botCounts.obsidianBot,
+                        state.botCounts.geodeBot
+                    )));
+                }
+
+                bestGeodesSoFar = Math.Max(bestGeodesSoFar, state.resources.geodes);
+                bestGeoBotsSoFar = Math.Max(bestGeoBotsSoFar, state.botCounts.geodeBot);
+                bestBotsSoFar = Math.Max(bestBotsSoFar, currStateBots);
+
+                foreach (var func in buildableBots)
+                { 
+                    ((int ore, int clay, int obsidian, int geodes) resources, (int, int, int, int) botCounts) newState = (
+                        (state.resources.ore, state.resources.clay, state.resources.obsidian, state.resources.geodes),
+                        (state.botCounts.oreBot, state.botCounts.clayBot, state.botCounts.obsidianBot, state.botCounts.geodeBot)
+                    );
+
+                    newState.resources.ore += state.botCounts.oreBot;
+                    newState.resources.clay += state.botCounts.clayBot;
+                    newState.resources.obsidian += state.botCounts.obsidianBot;
+                    newState.resources.geodes += state.botCounts.geodeBot;
+
+                    func(ref newState, blueprint);
+                    int newStateBots = state.botCounts.clayBot + state.botCounts.obsidianBot + state.botCounts.geodeBot;
+                    if (bestBotsSoFar <= newStateBots || bestBotsSoFar - newStateBots < 10)
+                    {
+                        nextStates.Add(newState);
+                    }
+                } 
+            }
+            allStates = nextStates;
+            nextStates = new();
+            allStates.RemoveWhere(state =>
+                bestGeodesSoFar - state.resources.geodes >= 2 ||
+                bestGeoBotsSoFar - state.botCounts.geodeBot >= 2
+            );
+        }
+
+        var bestState = allStates.MaxBy(x => x.resources.geodes);
+        return bestState.resources.geodes;
+    }
+
+    protected static void BuildOreBot(ref ((int ore, int, int, int) resources, (int oreBot, int, int, int) botCounts) state, int[] blueprint)
+    {
+        state.resources.ore -= blueprint[0];
+        state.botCounts.oreBot++;
+    }
+
+    protected static void BuildClayBot(ref ((int ore, int, int, int) resources, (int, int clayBot, int, int) botCounts) state, int[] blueprint)
+    {
+        state.resources.ore -= blueprint[1];
+        state.botCounts.clayBot++;
+    }
+
+    protected static void BuildObsidianBot(ref ((int ore, int clay, int, int) resources, (int, int, int obsidianBot, int) botCounts) state, int[] blueprint)
+    {
+        state.resources.ore -= blueprint[2];
+        state.resources.clay -= blueprint[3];
+        state.botCounts.obsidianBot++;
+    }
+
+    protected static void BuildGeodeBot(ref ((int ore, int, int obsidian, int) resources, (int, int, int, int geodeBot) botCounts) state, int[] blueprint)
+    {
+        state.resources.ore -= blueprint[4];
+        state.resources.obsidian -= blueprint[5];
+        state.botCounts.geodeBot++;
+    }
+
+
+    
+}
---
+++ 2022/Day20/Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Diagnostics;
+using Tools;
+
+Day20 day = new();
+day.OutputSecondStar();
+
+
+public class Day20 : DayBase
+{
+
+    public Day20() : base("20")
+    {
+
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetIntRowData();
+        var nodes = new List<LinkNode>();
+        LinkNode lastLinkNode = new();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            var node = new LinkNode { Value = data[i], Left = lastLinkNode };
+            if (lastLinkNode != null)
+                lastLinkNode.Right = node;
+            nodes.Add(node);
+            lastLinkNode = node;
+        }
+
+        var first = nodes[0];
+        first.Left = lastLinkNode;
+        lastLinkNode.Right = first;
+
+        for (int p = 0; p < nodes.Count; p++)
+        {
+            var node = nodes[p];
+            Debug.Assert(data[p] == node.Value);
+
+            if (Math.Abs(data[p]) % data.Length != 0)
+            {
+                for (int i = 0; i < Math.Abs(data[p]) % (data.Length - 1); i++)
+                {
+                    node = data[p] > 0 ? node.Right : node.Left;
+                }
+                var ogNode = nodes[p];
+                var left = ogNode.Left;
+                var right = ogNode.Right;
+                left.Right = right;
+                right.Left = left;
+                if (data[p] > 0)
+                {
+                    var temp = node.Right;
+                    node.Right = ogNode;
+                    ogNode.Left = node;
+                    ogNode.Right = temp;
+                    temp.Left = ogNode;
+                }
+                else
+                {
+                    var temp = node.Left;
+                    node.Left = ogNode;
+                    ogNode.Right = node;
+                    ogNode.Left = temp;
+                    temp.Right = ogNode;
+                }
+            }
+        }
+
+
+        var start = nodes.Single(n => n.Value == 0);
+        var sum = 0L;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 1000; j++)
+                start = start.Right;
+
+            sum += start.Value;
+        }
+
+        return sum.ToString();
+    }
+
+    public override string SecondStar()
+    {
+        const long key = 811589153;
+        var data = GetIntRowData().Select(r => r * key).ToArray();
+        var nodes = new List<LinkNode>();
+        LinkNode lastLinkNode = new();
+
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            var node = new LinkNode { Value = data[i], Left = lastLinkNode };
+            if (lastLinkNode != null)
+                lastLinkNode.Right = node;
+            nodes.Add(node);
+            lastLinkNode = node;
+        }
+
+        var first = nodes[0];
+        first.Left = lastLinkNode;
+        lastLinkNode.Right = first;
+
+        for (int s = 0; s < 10; s++)
+        {
+            for (int p = 0; p < nodes.Count; p++)
+            {
+                var node = nodes[p];
+                Debug.Assert(data[p] == node.Value);
+
+                if (Math.Abs(data[p]) % data.Length != 0)
+                {
+                    for (int i = 0; i < Math.Abs(data[p]) % (data.Length - 1); i++)
+                    {
+                        node = data[p] > 0 ? node.Right : node.Left;
+                    }
+                    var ogNode = nodes[p];
+                    var left = ogNode.Left;
+                    var right = ogNode.Right;
+                    left.Right = right;
+                    right.Left = left;
+                    if (data[p] > 0)
+                    {
+                        // for (int j = 0; j < data[p] / data.Length; j++)
+                        //     node = node.Right;
+                        var temp = node.Right;
+                        node.Right = ogNode;
+                        ogNode.Left = node;
+                        ogNode.Right = temp;
+                        temp.Left = ogNode;
+                    }
+                    else
+                    {
+                        // for (int j = 0; j < -data[p] / data.Length; j++)
+                        //     node = node.Left;
+                        var temp = node.Left;
+                        node.Left = ogNode;
+                        ogNode.Right = node;
+                        ogNode.Left = temp;
+                        temp.Right = ogNode;
+                    }
+                }
+            }
+        }
+
+        var start = nodes.Single(n => n.Value == 0);
+        var sum = 0L;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 1000; j++)
+                start = start.Right;
+
+            sum += start.Value;
+        }
+
+        return sum.ToString();
+    }
+}
+
+public class LinkNode
+{
+
+    public long Value { get; set; }
+
+    public LinkNode Left { get; set; }
+
+    public LinkNode Right { get; set; }
+
+}
