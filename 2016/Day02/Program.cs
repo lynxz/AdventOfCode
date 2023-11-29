a21161b3eb46c38ed891cd67716faeadad511fd3
anders.stromberg <anders.stroemberg@gmail.com>
2023-11-29 18:45:31 +0100

    2016 Day 2

---
+++ 2016/Day02/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using Tools;
+
+var day = new Day02();
+day.PostSecondStar();
+
+public class Day02 : DayBase
+{
+
+    public Day02() : base("2")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRowData();
+        var values = Enumerable.Range(1, 9).Select(x => x.ToString()).ToArray();
+        var result = new StringBuilder();
+        foreach (var row in data)
+        {
+            var x = 1;
+            var y = 1;
+            foreach (var c in row)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        y = Math.Max(0, y - 1);
+                        break;
+                    case 'D':
+                        y = Math.Min(2, y + 1);
+                        break;
+                    case 'L':
+                        x = Math.Max(0, x - 1);
+                        break;
+                    case 'R':
+                        x = Math.Min(2, x + 1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            result.Append(values![y * 3 + x]);
+        }
+
+        return result.ToString();
+    }
+
+    public override string SecondStar()
+    {
+        var data = GetRowData();
+        var values = new string?[5,5] {
+            { null, null, "1", null, null },
+            { null, "2", "3", "4", null },
+            { "5", "6", "7", "8", "9" },
+            { null, "A", "B", "C", null },
+            { null, null, "D", null, null }
+        };
+        var result = new StringBuilder();
+
+        foreach (var row in data)
+        {
+            var x = 0;
+            var y = 2;
+            foreach (var c in row)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        y = values[Math.Max(0, y - 1), x] == null ? y : Math.Max(0, y - 1);
+                        break;
+                    case 'D':
+                        y = values[Math.Min(4, y + 1), x] == null ? y : Math.Min(4, y + 1);
+                        break;
+                    case 'L':
+                        x = values[y, Math.Max(0, x - 1)] == null ? x : Math.Max(0, x - 1);
+                        break;
+                    case 'R':
+                        x = values[y, Math.Min(4, x + 1)] == null ? x : Math.Min(4, x + 1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            result.Append(values![y, x]);
+        }
+
+        return result.ToString();
+    }
+
+}
---
+++ 2022/Day25/Program.cs
@@ -0,0 +1,58 @@
+﻿using Tools;
+
+Day25 day = new();
+day.OutputFirstStar();
+
+public class Day25 : DayBase
+{
+
+    public Day25() : base("25")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRowData();
+        var max = data.Max(r => r.Length);
+        var mults = Enumerable.Range(0, max).Aggregate(new List<long>() { 1L }, (l, _) => AddReturn(5L * l.Last(), l));
+        var dict = new Dictionary<char, long> { { '0', 0L }, { '1', 1L }, { '2', 2L }, { '-', -1L }, { '=', -2L } };
+
+        var val = data.Select(r => Enumerable.Range(0, r.Length).Sum(i => mults[i] * dict[r[r.Length - 1 - i]])).ToList();
+        var total = val.Sum();
+
+       return SnafuFromDec(total);
+    }
+
+    static string SnafuFromDec(long value) {
+            List<char> arr = new();
+            while (value > 0) {
+                switch (value % 5) {
+                    case 3:
+                        arr.Add('=');
+                        value += 5;
+                        break;
+                    case 4:
+                        arr.Add('-');
+                        value += 5;
+                        break;
+                    default:
+                        arr.Add((char)((value % 5) + '0'));
+                        break;
+                }
+                value /= 5;
+            }
+            arr.Reverse();
+            return new string(arr.ToArray());
+        }
+
+    public static List<T> AddReturn<T>(T val, List<T> list)
+    {
+        list.Add(val);
+        return list;
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
