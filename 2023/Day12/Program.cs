31d6dccf82673e5c8032e79aa433151c8026b859
Anders Strömberg <anders.stromberg@viedoc.com>
2023-12-12 11:31:50 +0100

    Day12 complete

--- 2023/Day12/Program.cs
+++ 2023/Day12/Program.cs
@@ -1,7 +1,9 @@
-﻿using Tools;
+﻿using System.Runtime.InteropServices;
+using Tools;
 
 Day12 day = new();
-day.OutputFirstStar();
+//day.OutputFirstStar();
+day.OutputSecondStar();
 
 public class Day12 : DayBase
 {
@@ -12,53 +14,66 @@ public class Day12 : DayBase
     public override string FirstStar()
     {
         var data = GetRowData();
-
         var s = data.Select(r => (r.GetIntegers(), r.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0])).ToList();
 
-        foreach (var b in s)
-        {
-            Console.WriteLine($"{b.Item2} ({Combinations(b, 0, 0)})");
-        }
+        Enumerable.Range(0, s.Count).ToList().ForEach(i => System.Console.WriteLine($"{data[i]} {Combinations(s[i], 0, 0, new Dictionary<(int, int), long>())}"));
 
-
-        return s.Sum(spring => Combinations(spring, 0, 0)).ToString();
+        return s.Sum(spring => Combinations(spring, 0, 0, new Dictionary<(int, int), long>())).ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var data = GetRowData();
+        var s = data.Select(r => (Enumerable.Repeat(r.GetIntegers(), 5).SelectMany(x => x).ToArray(), string.Join('?', Enumerable.Repeat(r.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0], 5)))).ToList();
+
+        Enumerable.Range(0, s.Count).Take(15).ToList().ForEach(i => System.Console.WriteLine($"{data[i]} {Combinations(s[i], 0, 0, new Dictionary<(int, int), long>())}"));
+
+        return s.Sum(spring => (long)Combinations(spring, 0, 0, new Dictionary<(int, int), long>())).ToString();
     }
 
-    int Combinations((int[] i, string s) spring, int index, int pos)
+    long Combinations((int[] i, string s) spring, int index, int pos, Dictionary<(int, int), long> memo)
     {
         var (i, s) = spring;
+        if (index >= i.Length && pos < s.Length && Enumerable.Range(pos, s.Length - pos).Any(j => s[j] == '#'))
+            return 0;
         if (index >= i.Length)
             return 1;
         if (i[index] > s.Length - pos)
             return 0;
         if (i[index] == s.Length - pos && index == i.Length - 1)
-            return 1;
+            return Enumerable.Range(pos, s.Length - pos).All(j => s[j] == '#' || s[j] == '?') ? 1 : 0;
 
         var left = i.Skip(index + 1).Sum();
         var offset = 0;
-        var result = 0;
+        var result = 0L;
         do
         {
-
-            var s2 = s.Substring(pos + offset, i[index]);
-            if (s2.All(c => c == '#' || c == '?'))
+            if (memo.TryGetValue((pos + offset, index), out var val))
             {
-                if (pos + offset + i[index] == s.Length)
+                result += val;
+            }
+            else
+            {
+                var s2 = s.Substring(pos + offset, i[index]);
+                if (s2.All(c => c == '#' || c == '?'))
                 {
-                    result += Combinations(spring, index + 1, pos + offset + i[index] + 1);
-                }
-                else if (pos + offset + i[index] < s.Length && s[pos + offset + i[index]] != '#')
-                {
-                    result += Combinations(spring, index + 1, pos + offset + i[index] + 1);
+                    if (pos + offset + i[index] == s.Length)
+                    {
+                        var combs = Combinations(spring, index + 1, pos + offset + i[index] + 1, memo);
+                        memo.Add((pos + offset, index), combs);
+                        result += combs;
+                    }
+                    else if (pos + offset + i[index] < s.Length && s[pos + offset + i[index]] != '#')
+                    {
+                        var combs = Combinations(spring, index + 1, pos + offset + i[index] + 1, memo);
+                        memo.Add((pos + offset, index), combs);
+                        result += combs;
+                    }
                 }
             }
+
             offset++;
-        } while (offset + pos + left < s.Length - 1 && offset + pos + i[index] < s.Length);
+        } while (offset + pos + left < s.Length && offset + pos + i[index] <= s.Length && Enumerable.Range(pos, offset).All(j => s[j] != '#'));
 
         return result;
     }
