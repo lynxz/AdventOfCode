ee7b1ae298d9483d0b1a0c61d0cf9ce587da91ab
anders.stromberg <anders.stroemberg@gmail.com>
2024-12-09 23:18:13 +0100

    Day 9 completed

--- 2024/Day09/Program.cs
+++ 2024/Day09/Program.cs
@@ -2,7 +2,7 @@
 using Tools;
 
 var day = new Day09();
-day.OutputFirstStar();
+day.OutputSecondStar();
 
 public class Day09 : DayBase
 {
@@ -30,7 +30,8 @@ public class Day09 : DayBase
             files[filePos] -= m;
             space[freeIndex] -= m;
 
-            if (files[filePos] == 0) {
+            if (files[filePos] == 0)
+            {
                 filePos--;
             }
             if (space[freeIndex] == 0)
@@ -60,6 +61,47 @@ public class Day09 : DayBase
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        var data = GetRawData().Trim();
+        var files = new List<(int id, int pos, int count)>();
+        var spaces = new List<(int pos, int count)>();
+        int c = 0;
+        _ = data.Aggregate(0, (pos, x) =>
+        {
+            var v = x - '0';
+            if (c % 2 != 0)
+            {
+                spaces.Add((pos, v));
+            }
+            else
+            {
+                files.Add((c / 2, pos, v));
+            }
+
+            pos += v;
+            c++;
+            return pos;
+        });
+
+        var filePos = files.Count - 1;
+        var freeIndex = spaces.FindIndex(s => s.count >= files[filePos].count);
+        while (0 < filePos - 1)
+        {
+
+            if (freeIndex != -1 && spaces[freeIndex].pos < files[filePos].pos)
+            {
+                files[filePos] = (files[filePos].id, spaces[freeIndex].pos, files[filePos].count);
+                spaces[freeIndex] = (spaces[freeIndex].pos + files[filePos].count, spaces[freeIndex].count - files[filePos].count);
+            }
+            filePos--;
+            freeIndex = spaces.FindIndex(s => s.count >= files[filePos].count);
+        }
+
+        return files.Aggregate(new BigInteger(0), (sum, file) =>
+            {
+                Enumerable.Range(file.pos, file.count).ToList().ForEach(j => sum += j * file.id);
+                return sum;
+            }).ToString();
     }
+
+
 }
