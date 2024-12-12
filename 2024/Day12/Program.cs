f3582554df182bdf407db13678a2a62e0d417a1d
Anders Strömberg <anders.stromberg@viedoc.com>
2024-12-12 17:17:33 +0100

    day 12 still going

--- 2024/Day12/Program.cs
+++ 2024/Day12/Program.cs
@@ -98,10 +98,10 @@ public class Day12 : DayBase
 
             var crop = data[y][x];
             var area = 0;
+            var perimeter = new List<(int x, int y)>();
             var plotPoints = new Stack<(int x, int y)>();
             plotPoints.Push((x, y));
 
-            var boundary = 0;
             while (plotPoints.Count > 0)
             {
                 var (px, py) = plotPoints.Pop();
@@ -111,7 +111,10 @@ public class Day12 : DayBase
                     var ny = py + dy;
                     if (nx < 0 || nx >= xMax || ny < 0 || ny >= yMax || data[ny][nx] != crop)
                     {
-                        boundary++;
+                        if (!perimeter.Contains((px, py)))
+                        {
+                            perimeter.Add((px, py));
+                        }
                         if (nx >= 0 && nx < xMax && ny >= 0 && ny < yMax)
                         {
                             stack.Push((nx, ny));
@@ -127,8 +130,62 @@ public class Day12 : DayBase
                 area++;
                 processed.Add((px, py));
             }
-            System.Console.WriteLine($"Crop: {crop}, Boundary: {boundary}, Area: {area}");
-            totalSum += area * boundary;
+            var sides = WalkPerimeter(perimeter, data, xMax, yMax);
+            System.Console.WriteLine($"Crop: {crop}, Sides: {sides}, Area: {area}");
+            totalSum += area * sides;
         }
+
+        return totalSum.ToString();
+    }
+
+    List<(int x, int y)> Dirs = new List<(int x, int y)>()
+    {
+        (1, 0),
+        (0, 1),
+        (-1, 0),
+        (0, -1),
+    };
+
+    private int WalkPerimeter(List<(int x, int y)> perimeter, string[] data, int xMax, int yMax)
+    {
+        var visited = new HashSet<(int x, int y)>();
+        var sides = 0;
+        var start = perimeter.OrderBy(c => c.y).ThenBy(c => c.x).First();
+        var crop = data[start.y][start.x];
+
+        var outDir = Dirs.First(d => d.x + start.x < 0 || d.x + start.x >= xMax || d.y + start.y < 0 || d.y + start.y >= yMax || data[d.y + start.y][d.x + start.x] != crop);
+        var outDirIndex = Dirs.IndexOf(outDir);
+        var startDirIndex = (outDirIndex + 1) % Dirs.Count;
+
+        var pos = start;
+        var currDirIndex = startDirIndex;
+        do
+        {
+            visited.Add(pos);
+            var od = Dirs[outDirIndex];
+            var dx = pos.x + od.x;
+            var dy = pos.y + od.y;
+            if (dx >= 0 && dx < xMax && dy >= 0 && dy < yMax && data[dy][dx] == crop)
+            {
+                sides++;
+                currDirIndex = (currDirIndex + 3) % Dirs.Count; 
+                outDirIndex = (outDirIndex + 3) % Dirs.Count; 
+                continue;
+            }
+            od = Dirs[currDirIndex];
+            dx = pos.x + od.x;
+            dy = pos.y + od.y;
+            if (dx < 0 || dx >= xMax || dy < 0 || dy >= yMax && data[dy][dx] != crop)
+            {
+                sides++;
+                currDirIndex = (currDirIndex + 1) % Dirs.Count;
+                outDirIndex = (outDirIndex + 1) % Dirs.Count;
+                continue;
+            }
+            pos = (dx, dy);
+
+        } while (!(pos == start && currDirIndex == startDirIndex));
+
+        return sides;
     }
 }
