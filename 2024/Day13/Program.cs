df4d869ccbab8e29cd81c7f68ae56684ace85a3b
anders.stromberg <anders.stroemberg@gmail.com>
2024-12-13 07:10:47 +0100

    Day 13 started

---
+++ 2024/Day13/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tools;
+
+var day = new Day13();
+day.OutputFirstStar();
+
+public class Day13 : DayBase
+{
+    public Day13() : base("13")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRawData();
+        var machineData = data.Split("\n\n");
+        var sum = 0;
+        foreach(var machine in machineData)
+        {
+            var lines = machine.Split("\n");
+            var buttonA = lines[0].GetIntegers();
+            var buttonB = lines[1].GetIntegers();
+            var prize = lines[2].GetIntegers();
+            
+            var a =0;
+            var b = 0;
+
+            
+            System.Console.WriteLine(string.Join(",", buttonA));
+            System.Console.WriteLine(string.Join(",", buttonB));
+            System.Console.WriteLine(string.Join(",", prize));
+            System.Console.WriteLine();
+            
+        }
+
+        return sum.ToString();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
