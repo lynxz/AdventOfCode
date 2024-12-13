2250569a4e3d9680c2f2f1845df6ecfbbd5b8875
Anders Strömberg <anders.stromberg@viedoc.com>
2024-12-13 08:29:32 +0100

    day 13 complete

--- 2024/Day13/Program.cs
+++ 2024/Day13/Program.cs
@@ -1,7 +1,9 @@
-﻿using Tools;
+﻿using System.Numerics;
+using Tools;
 
 var day = new Day13();
 day.OutputFirstStar();
+day.OutputSecondStar();
 
 public class Day13 : DayBase
 {
@@ -11,32 +13,38 @@ public class Day13 : DayBase
 
     public override string FirstStar()
     {
-        var data = GetRawData();
-        var machineData = data.Split("\n\n");
-        var sum = 0;
-        foreach(var machine in machineData)
-        {
-            var lines = machine.Split("\n");
-            var buttonA = lines[0].GetIntegers();
-            var buttonB = lines[1].GetIntegers();
-            var prize = lines[2].GetIntegers();
-            
-            var a =0;
-            var b = 0;
-
-            
-            System.Console.WriteLine(string.Join(",", buttonA));
-            System.Console.WriteLine(string.Join(",", buttonB));
-            System.Console.WriteLine(string.Join(",", prize));
-            System.Console.WriteLine();
-            
-        }
-
-        return sum.ToString();
+        return CalculateTokens(true).ToString();
     }
 
     public override string SecondStar()
     {
-        throw new NotImplementedException();
+        return CalculateTokens(false).ToString();
+    }
+
+    private BigInteger CalculateTokens(bool first)
+    {
+        var data = GetRawData();
+        var machineData = data.Split("\n\n");
+        var sum = new BigInteger(0);
+        foreach (var machine in machineData)
+        {
+            var lines = machine.Split("\n");
+            var buttonA = lines[0].GetIntegers().Select(i => new BigInteger(i)).ToArray();
+            var buttonB = lines[1].GetIntegers().Select(i => new BigInteger(i)).ToArray();
+            var prize = lines[2].GetIntegers().Select(i => new BigInteger(first ? i : 10000000000000 + i)).ToArray();
+
+
+            if (BigInteger.Abs(buttonA[1] * prize[0] - buttonA[0] * prize[1]) % BigInteger.Abs(buttonA[1] * buttonB[0] - buttonA[0] * buttonB[1]) == 0)
+            {
+                var b = (buttonA[1] * prize[0] - buttonA[0] * prize[1]) / (buttonA[1] * buttonB[0] - buttonA[0] * buttonB[1]);
+                if (BigInteger.Abs(prize[1] - buttonB[1] * b) % buttonA[1] == 0)
+                {
+                    var a = (prize[1] - buttonB[1] * b) / buttonA[1];
+                    sum += a * 3 + b;
+                }
+            }
+        }
+
+        return sum;
     }
 }
