243645867b8fc0c47d6498a2b7b48489c8582663
Anders Strömberg <anders.stromberg@semantix.com>
2025-12-05 08:59:04 +0100

    More refactoring

--- 2025/Day04/Program.cs
+++ 2025/Day04/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices.Marshalling;
-using Tools;
+﻿using Tools;
 
 var day = new Day04();
 day.OutputFirstStar();
@@ -7,63 +6,44 @@ day.OutputSecondStar();
 
 public class Day04 : DayBase
 {
+    (int x, int y)[] _offsets = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
+
     public Day04() : base("4")
     {
     }
 
     public override string FirstStar()
     {
-        var rows = GetRowData().Where(r => !string.IsNullOrWhiteSpace(r)).Select(s => s.Trim().ToCharArray());
-        var xMax = rows.First().Length;
-        var all = rows.SelectMany(r => r).ToArray();
-        var t = all.Count();
-        var yMax = t / xMax;
-        (int x, int y)[] offsets = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
-
-        return Enumerable.Range(0, t).Where(i =>
-        {
-            var x = i % xMax;
-            var y = i / xMax;
-            if (all[i] != '@') return false;
-            return offsets
-                .Where(o => x + o.x < xMax && y + o.y < yMax && y + o.y >= 0 && x + o.x >= 0)
-                .Count(o => all[(y + o.y) * xMax + x + o.x] == '@') < 4;
-        }).Count()
-        .ToString();
+        var set = GetHashSet();
+        return set
+            .Count(c => _offsets.Count(o => set.Contains((c.x + o.x, c.y + o.y))) < 4)
+            .ToString();
     }
 
     public override string SecondStar()
     {
-        var rows = GetRowData().Where(r => !string.IsNullOrWhiteSpace(r)).Select(s => s.Trim().ToCharArray());
-        var xMax = rows.First().Length;
-        var all = rows.SelectMany(r => r).ToArray();
-        var t = all.Length;
-        var yMax = t / xMax;
-        (int x, int y)[] offsets = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
-
+        var set = GetHashSet();
         var totalSum = 0;
-        var sum = 0;
+        var toRemove = Array.Empty<(int x, int y)>();
         do
         {
-            var copy = new char[t];
-            sum = Enumerable.Range(0, t).Where(i =>
-            {
-                copy[i] = all[i];
-                var x = i % xMax;
-                var y = i / xMax;
-                if (all[i] != '@') return false;
-                var rolls = offsets
-                    .Where(o => x + o.x < xMax && y + o.y < yMax && y + o.y >= 0 && x + o.x >= 0)
-                    .Count(o => all[(y + o.y) * xMax + x + o.x] == '@') < 4;
-                if (rolls)
-                    copy[i] = '.';
-
-                return rolls;
-            }).Count();
-            all = copy;
-            totalSum += sum;
-        } while (sum > 0);
+            toRemove = set
+                .Where(c => _offsets.Count(o => set.Contains((c.x + o.x, c.y + o.y))) < 4)
+                .ToArray();
+            totalSum += toRemove.Length;
+            set = set.Except(toRemove).ToHashSet();
+        } while (toRemove.Length > 0);
 
         return totalSum.ToString();
+    }
+
+    private HashSet<(int x, int y)> GetHashSet()
+    {
+        var rows = GetRowData().Where(r => !string.IsNullOrWhiteSpace(r)).Select(s => s.Trim().ToCharArray()).ToList();
+        var xMax = rows.First().Length;
+        var yMax = rows.Count;
+
+        HashSet<(int x, int y)> set = [.. Enumerable.Range(0, xMax).SelectMany(x => Enumerable.Range(0, yMax).Where(y => rows[y][x] == '@').Select(y => (x, y)))];
+        return set;
     }
 }
