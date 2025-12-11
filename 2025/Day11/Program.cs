7a4a38fd6751251dc9ada5fbd49b8a58dbb719e1
Anders Strömberg <anders.stromberg@semantix.com>
2025-12-11 08:31:34 +0100

    Day 11 complete

--- 2025/Day11/Program.cs
+++ 2025/Day11/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Tools;
 
 Day11 day = new();
+day.OutputFirstStar();
 day.OutputSecondStar();
 
 public class Day11 : DayBase
@@ -29,13 +30,9 @@ public class Day11 : DayBase
             foreach (var neighbor in graph[current])
             {
                 if (neighbor == "out")
-                {
                     paths++;
-                }
                 else if (neighbor != "you")
-                {
                     stack.Push(neighbor);
-                }
             }
         }
 
@@ -50,35 +47,50 @@ public class Day11 : DayBase
         {
             graph[line[..3]] = line[5..].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToArray();
         }
+        var result = DFS(graph, "svr", new Dictionary<string, long[]>());
 
-        var stack = new Stack<(string Node, List<string> Path)>();
-        stack.Push(("svr", new List<string> { "svr" }));
-        var paths = 0;
+        return result[3].ToString();
+    }
 
-        while (stack.Count > 0)
+    private long[] DFS(Dictionary<string, string[]> graph, string current, Dictionary<string, long[]> memo)
+    {
+        var paths = new long[4]; // [0] = no dac/fft, [1] = dac only, [2] = fft only, [3] = both
+
+        foreach (var neighbor in graph[current])
         {
-            var current = stack.Pop();
-
-            foreach (var neighbor in graph[current.Node])
+            if (neighbor == "out")
             {
-                if (neighbor == "out")
-                {
-                    System.Console.WriteLine("OUT!");
-                    if (current.Path.Contains("dac") && current.Path.Contains("fft"))
-                    {
-                        System.Console.WriteLine(string.Join(" -> ", current.Path) + " -> out");
-                        paths++;
-                    }
-                        
-                }
-                else if (!current.Path.Contains(neighbor))
-                {
-                    var newPath = new List<string>(current.Path) { current.Node };
-                    stack.Push((neighbor, newPath));
-                }
+                if (current == "dac")
+                    paths[1]++;
+                else if (current == "fft")
+                    paths[2]++;
+                else
+                    paths[0]++;
+    
+                continue;
+            }
+            var result = memo.ContainsKey(neighbor) ? memo[neighbor] : DFS(graph, neighbor, memo);
+
+            if (current == "dac")
+            {
+                paths[1] += result[0];
+                paths[3] += result[2] + result[3];
+            }
+            else if (current == "fft")
+            {
+                paths[2] += result[0];
+                paths[3] += result[1] + result[3];
+            } 
+            else
+            {
+                paths[0] += result[0];
+                paths[1] += result[1];
+                paths[2] += result[2];
+                paths[3] += result[3];
             }
         }
+        memo[current] = paths;
 
-        return paths.ToString();
+        return paths;
     }
 }
