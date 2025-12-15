2c43e7112e87b255f41901ce329f417e200e76be
Anders Strömberg <anders.stromberg@semantix.com>
2025-12-15 08:19:32 +0100

    Day 12 complete

---
+++ 2025/Day12/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tools;
+
+Day12 day = new();
+day.OutputFirstStar();
+
+public class Day12 : DayBase
+{
+    public Day12() : base("12")
+    {
+    }
+
+    public override string FirstStar()
+    {
+        var data = GetRawData().Replace("\r", "").Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var sum = 0;
+        foreach(var row in data[6].Split("\n", StringSplitOptions.RemoveEmptyEntries))
+        {
+            var colon = row.IndexOf(':');
+            var size = row[..colon].Trim().Split('x').Select(int.Parse).ToArray();
+            var numberOfPackages = row[(colon + 1)..].Trim().Split(' ').Select(int.Parse).ToArray();
+            var amnt = numberOfPackages.Aggregate(size[0] / 3 * (size[1] / 3), (amnt, packages) => amnt - packages);
+            if (amnt >= 0)
+                sum++;
+        }
+
+        return sum.ToString();
+    }
+
+    public override string SecondStar()
+    {
+        throw new NotImplementedException();
+    }
+}
